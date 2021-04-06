6bf89fa76d77c60c02bc47047cb1d968d570b7c8
arifhusaini97 <35693271+arifhusaini97@users.noreply.github.com>
2021-04-07 07:54:20 +0800

    Initialization
    Basic ASP.Net Core Web API (Completed) (Code-Maze)

---
+++ AccountOwnerServer/AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using System;
+
+namespace AccountOwnerServer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Account, AccountDto>();
+            CreateMap<OwnerForCreationDto, Owner>();
+            CreateMap<OwnerForUpdateDto, Owner>();
+        }
+    }
+}
